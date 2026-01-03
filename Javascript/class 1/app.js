b1ad917b8c2bf4f367d68080b900ea936a080df9
Mohammad Saad Ali <mohammadsaadali786.92@gmail.com>
2026-01-03 17:40:11 +0500

    3 jan 2026

---
+++ Javascript/class 1/app.js
@@ -0,0 +1,31 @@
+// document.writeln('<h1> hello <br> world </h1>')
+
+// document.writeln('Laiba')
+// document.writeln('Laiba')
+// document.writeln('Laiba')
+// document.writeln('Laiba')
+// document.writeln('Laiba')
+// document.writeln('Laiba')
+// document.writeln('Laiba')
+// document.writeln('Laiba')
+// document.writeln('Laiba')
+// document.writeln('Laiba')
+
+
+// var user = 'laiba '
+// var age = 20
+// document.writeln(user, age)
+// document.writeln(user , " ", age)
+
+
+
+// var admin = "Laiba"
+// var age = 70
+// document.writeln("Your name is ", admin, "<br> Your age is ", age)
+
+var num1 = 10
+var num2 = "glass"
+
+document.writeln(num1 * num2)
+
+
