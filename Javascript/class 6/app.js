f972ddbde0b96c46ff39fbba64e121bcf346bb6f
Mohammad Saad Ali <mohammadsaadali786.92@gmail.com>
2026-01-31 12:53:51 +0500

    31 jan 2026

---
+++ Javascript/class 6/app.js
@@ -0,0 +1,86 @@
+// var a = 5
+// var b = 3
+
+// if(a == 5){
+//     if(b == 1){
+//         console.log('both are true')
+//     }
+//     else{
+//         console.log('inner is false')
+//     }
+// }
+// else {
+//     console.log('outer is false')
+// }
+
+// var user = prompt('Name')
+// var password = prompt('Password')
+
+// if(user == 'admin'){
+//     if(password == '123456'){
+//         console.log('login success')
+//     }
+//     else{
+
+//     }
+// }
+// else{
+// }
+
+
+
+// var studName = "Saad"
+// var rollNo = '12345'
+// var sec = "B"
+// var cnic = "0987"
+
+
+// var abc;
+// console.log(abc)
+
+// var student = ['ABCD', 10, false, undefined, 15.4]
+
+// student[0] = '123'
+// student[5] = 'xyz'
+// console.log(student[0])
+
+
+var fruits = ['apple', 'mango', 'orange', 'banana', 'kiwi']
+
+fruits.push("abcd", "xyz")
+fruits.pop()
+fruits.unshift("123", "ghi")
+fruits.shift()
+fruits.shift()
+fruits.shift()
+
+console.log(fruits)
+
+
+// fruits[5] = 'abcd'
+// fruits[5] = ''
+
+// console.log(fruits.length)
+
+
+
+// push / akhir se add
+// pop  /  akhir se delete
+// unshift / shru se add
+// shift  / shru se delete
+// slice / copy array k elements or new array
+// splice / kahin se bhi add kahin se bhi del
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Javascript/class 7/app.js
@@ -0,0 +1,40 @@
+// for (var i = 1; i <= 50; i++){
+//     document.writeln(i + "<br>")
+// }
+
+// for(var i=1; i <=10; i++){
+//     document.writeln(i + ") Majeed SRE <br>")
+// }
+
+// for(var i=10; i > 0; i-- ){
+//     document.writeln(i + "<br>")
+// }
+
+// for(var i=1; i <= 10; i += 2){
+//     document.writeln(i + "<br>")
+// }
+
+// for(var i=2; i <= 20; i+=2){
+//     document.writeln(i + "<br>")
+// }
+
+// for (var i = 1; i <= 10; i++) {
+//     document.writeln(
+//         `5 x ${i} = ${5 * i} <br>`
+//     )
+// }
+
+var tableNum = prompt("Enter your table number")
+
+for(var i=1; i<=10; i++){
+    document.writeln(
+        `${tableNum} x ${i} = ${tableNum * i} <br>`
+    )
+}
+
+
+
+
+
+
+
