444d2182a9b17bd930bd7888d6d3411ef51042b4
Mohammad Saad Ali <mohammadsaadali786.92@gmail.com>
2026-02-21 16:43:25 +0500

    21 feb 2026

---
+++ Javascript/class 12/app.js
@@ -0,0 +1,56 @@
+// function print(){
+//     console.log("hello world")
+// }
+
+// function table(){
+//     for(var i=1; i <= 10; i++){
+//         document.writeln(`2 x ${i} = ${i * 2} <br>`)
+//     }
+// }
+
+// table()
+// table()
+
+
+
+
+// function sum(a, b){
+//     return a + true + "hello"
+// }
+// var getValue = sum(10)
+
+// document.writeln(getValue)
+// document.writeln(getValue2)
+
+
+// function check(){
+//     var i = 10
+// }
+
+// console.log(i)
+// check()
+
+
+function calc(){
+    var a = +prompt("Enter value 1")
+    var b = +prompt("Enter value 2")
+    var c = prompt("Enter operator")
+
+    if(c == "+"){
+        console.log(a + b)
+    }
+    else if(c == '-'){
+        console.log(a - b)
+    }
+}
+
+calc()
+
+
+
+
+
+
+
+
+
